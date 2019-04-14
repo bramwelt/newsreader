51d1b9b88c6b5c6dd248ed1077c89f231bdc1359
Trevor Bramwell <trevor@bramwell.net>
2019-04-14 06:32:36 -0700

    Update parsing for NPR articles
    This change brings a much needed refresh to the way NPR articles are
    displayed, and lays the ground work for allowing users to open external
    links in their browser of choice.
    
     - Emphasised tags are show in italics
       Note: Due to missing support in urwid, italics are not actually
       rendered
     - Links are underlined and a reference number is listed next to them
     - Headers and strong tags are show in bold
    
    Also fixes a bug where clicking would accidentally quit the application.
    
    Signed-off-by: Trevor Bramwell <trevor@bramwell.net>

--- nr/site.py
+++ nr/site.py
@@ -13,6 +13,7 @@ from .article import Article
 class Site():
     """A site represents a news source and contains a list of articles
        after being parsed"""
+    name = "NPR"
 
     def __init__(self, url):
         self.url = url
@@ -22,12 +23,12 @@ class Site():
         """Parse a site and extract the articles"""
         # logging.info("Parsing: %s", self.url)
         page = requests.get(self.url)
-        soup = BeautifulSoup(page.content, 'html.parser')
+        soup = BeautifulSoup(page.content, 'html5lib')
         links = soup.body.ul.find_all('li')
         for link in links:
             # logging.info("Adding article: %s", link)
             href = "%s%s" % (self.url, link.a['href'])
-            article = Article(link.text, href)
+            article = Article(self.name, link.string, href)
             self.articles.append(article)
 
     def get_articles(self):
